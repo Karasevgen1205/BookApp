72f80b2a5d8c9c180821002bdf685261f1945be8
Yauheni <Karasevgen1205@yandex.ru>
2021-02-01 22:22:50 +0300

    add startapp manage_book 3

--- book_app/manage_book/models.py
+++ book_app/manage_book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class UserPhone(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE(),
+
+    )
