66eeda8b181b40701608c8a4572948f2c8f4c140
Yauheni <Karasevgen1205@yandex.ru>
2021-02-03 23:18:15 +0300

    add startapp manage_book 12

--- book_app/book_app/urls.py
+++ book_app/book_app/urls.py
@@ -1,8 +1,4 @@
-"""book_app URL Configuration
 
-e include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
