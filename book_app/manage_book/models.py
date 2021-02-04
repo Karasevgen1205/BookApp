f45c6112f4d30039183210caf16962e19d2808d4
Yauheni <Karasevgen1205@yandex.ru>
2021-02-04 18:58:22 +0300

    add startapp manage_book 17

--- book_app/manage_book/models.py
+++ book_app/manage_book/models.py
@@ -16,3 +16,8 @@ class UserPhone(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
